e261762dd442e2a12faf140012ae62c859638491
Ayushmaan Ganguli <ayushmaanganguli64@gmail.com>
2026-04-29 00:41:34 +0530

    Q2

---
+++ Q2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SubstringProgram {
+
+    // Create substring using charAt()
+    static String createSubstring(String str, int start, int end) {
+
+        String result = "";
+
+        for (int i = start; i < end; i++) {
+            result += str.charAt(i);
+        }
+
+        return result;
+    }
+
+    // Compare two strings using charAt()
+    static boolean compareStrings(String s1, String s2) {
+
+        if (s1.length() != s2.length())
+            return false;
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String text = sc.next();
+
+        System.out.print("Enter start index: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end index: ");
+        int end = sc.nextInt();
+
+        String userSub = createSubstring(text, start, end);
+        String builtInSub = text.substring(start, end);
+
+        boolean result = compareStrings(userSub, builtInSub);
+
+        System.out.println("Substring using charAt(): " + userSub);
+        System.out.println("Substring using substring(): " + builtInSub);
+        System.out.println("Are both equal? " + result);
+    }
+}
